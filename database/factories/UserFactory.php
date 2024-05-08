dadfc0ac9ac0f1d42523ca25fafda319949c3102
Rosta <martyniv@seznam.cz>
2024-05-08 09:14:57 +0200

    crm

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,13 +21,13 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition() : array
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('123'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -35,7 +35,7 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): static
+    public function unverified() : static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run() : void
     {
-        // User::factory(10)->create();
+        User::factory(1000)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'martyniv',
+            'email' => 'martyniv@seznam.cz',
         ]);
     }
 }
